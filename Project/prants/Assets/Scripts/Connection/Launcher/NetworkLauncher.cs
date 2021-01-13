c4e848516fd0c651ad39413d2196e2bd5a68984c
Joao Rabello <rabellois@hotmail.com>
2021-01-13 12:40:28 -0300

    NetworkLauncher created + Connection with master server working

---
+++ Project/prants/Assets/Scripts/Connection/Launcher/NetworkLauncher.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class NetworkLauncher : MonoBehaviourPunCallbacks
+{
+    public static NetworkLauncher Instance;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.NickName = "Jogador " + Random.Range(0, 1000).ToString("0000");
+        // MenuManager.Instance.OpenMenu(1);
+    }
+    
+    // public void CreateRoom()
+    // {
+    //     if (string.IsNullOrEmpty(_createRoomNameInputField.text)) return;
+    //     
+    //     PhotonNetwork.CreateRoom(_createRoomNameInputField.text);
+    //     MenuManager.Instance.OpenMenu(0);
+    // }
+}
