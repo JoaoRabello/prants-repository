7900dfe3e01cf3e16c4c68257b149b8aad1f235d
Joao Rabello <rabellois@hotmail.com>
2021-01-14 01:15:47 -0300

    Character Menu started

---
+++ Project/prants/Assets/Scripts/Menu Control/CharacterListItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterListItem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _characterNameLabel;
+    [SerializeField] private TMP_Text _characterDetailsLabel;
+}
